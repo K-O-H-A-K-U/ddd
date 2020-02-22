17f9607902e7e30fc1703343b842b8a7a290eabc
K-O-H-A-K-U <schwartzekatze1996510@gmail.com>
2020-02-23 00:15:20 +0900

    improve

--- js/script.js
+++ js/script.js
@@ -12,15 +12,35 @@ window.addEventListener('load', function(){
 {
     let elm = document.getElementsByClassName('toggle');
     Array.prototype.forEach.call(elm, element => {
-        element.addEventListener('click', (e) => {
-            let article = e.target;
-            while (article.nodeName != 'ARTICLE') {
-                article = article.parentNode;
-            }
-            // nodeType 1 is ELEMENT_NODE
-            let content = Array.prototype.map.call(article.childNodes, element => element)
-                                             .find(node => node.nodeType == 1 && node.classList.contains('content'));
-            content.style.display = (content.style.display == 'none') ? 'block' : 'none';
-        });
+        element.addEventListener('click', e => toggle_move(e));
     });
+
+    const toggle_move = event => {
+        toggle_content(event.target).then(value => scroll_y(value));
+    }
+
+    const scroll_y = y => {
+        window.scroll({
+            top: y,
+            behavior: 'smooth'
+        })
+    }
+    const toggle_content = (target) => {
+        let article = target;
+        while (article.nodeName != 'ARTICLE') {
+            article = article.parentNode;
+        }
+        // nodeType 1 is ELEMENT_NODE
+        let article_top = article.getBoundingClientRect().y;
+        const content = Array.prototype.map.call(article.childNodes, element => element)
+                                         .find(node => node.nodeType == 1 && node.classList.contains('content'));
+        if (content.style.display == 'none') {
+            Array.prototype.forEach.call(document.getElementsByClassName('content'), content => content.style.display = 'none');
+            content.style.display = 'block';
+            return Promise.resolve(article_top);
+        } else {
+            content.style.display = 'none'
+            return Promise.resolve(0);
+        }
+    }
 }
