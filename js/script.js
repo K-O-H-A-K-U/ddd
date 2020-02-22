d1b31b35593193c262c66ff8aa0af3cdc7c96c98
K-O-H-A-K-U <schwartzekatze1996510@gmail.com>
2020-02-22 22:52:07 +0900

    fix embarrassing code

--- js/script.js
+++ js/script.js
@@ -1,68 +1,26 @@
-function puldown(event) {
-	obj=document.getElementsByClassName(event.target.eventParam);
-	obj[0].style.display=(obj[0].style.display=='none')?'block':'none';
-}
-
 window.addEventListener('load', function(){
-				var elm = document.getElementsByClassName("pulled");
-				for (let i = 0; i<elm.length; i++){
-					(function(n){
-						elm[n].style.display="none";
-					})(i);
-				}
+    var elm = document.getElementsByClassName("content");
+    for (let i = 0; i<elm.length; i++){
+        (function(n){
+            elm[n].style.display="none";
+        })(i);
+    }
 
-				document.body.style.visibility="visible";	
-			}, false);
-
-{
-	let elm = document.getElementsByClassName('chef_pulling');
-	for(let i=0;i<elm.length;i++){
-		(function(n){
-			elm[n].addEventListener('click', puldown, false);
-		elm[n].eventParam='chef_pul';
-		})(i);
-	}
-}	
-
-{
-	let elm = document.getElementsByClassName('bento_pulling');
-	for(let i=0;i<elm.length;i++){
-		(function(n){
-			elm[n].addEventListener('click', puldown, false);
-		elm[n].eventParam='bento_pul';
-		})(i);
-	}
-}
-
-{
-	let elm = document.getElementsByClassName('events_pulling');
-	for(let i=0;i<elm.length;i++){
-		(function(n){
-			elm[n].addEventListener('click', puldown, false);
-		elm[n].eventParam='events_pul';
-		})(i);
-	}
-}
-
-{
-let elm  = document.getElementsByClassName('hum_pulling');
-elm[0].addEventListener('click', function(){
-	obj=document.getElementsByClassName('hum_pul');
-	obj[0].style.display='block';
-	elm[0].style.display='none';
+    document.body.style.visibility="visible";
 }, false);
-}
 
 {
-let elm  = document.getElementsByClassName('close');
-for(let i=0; i<elm.length; i++){
-	(function(n){
-		elm[n].addEventListener('click', function(){
-			obj=document.getElementsByClassName('hum_pul');
-			obj[0].style.display='none';
-			let pul = document.getElementsByClassName('hum_pulling');
-			pul[0].style.display='block';
-		}, false);
-	})(i);
-}
+    let elm = document.getElementsByClassName('toggle');
+    Array.prototype.forEach.call(elm, element => {
+        element.addEventListener('click', (e) => {
+            let article = e.target;
+            while (article.nodeName != 'ARTICLE') {
+                article = article.parentNode;
+            }
+            // nodeType 1 is ELEMENT_NODE
+            let content = Array.prototype.map.call(article.childNodes, element => element)
+                                             .find(node => node.nodeType == 1 && node.classList.contains('content'));
+            content.style.display = (content.style.display == 'none') ? 'block' : 'none';
+        });
+    });
 }
